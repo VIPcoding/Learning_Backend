7114f3115c008441d0d2e6d799eaf739aa4cf201
Vipin <vipsachdeva1@gmail.com>
2026-02-06 21:26:05 +0530

    creating server

---
+++ Day_01/app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const app = express()
+
+
+app.get('/', (req, res)=>{
+    console.log("hello world")
+    res.send("hello world from seerver")
+})
+
+
+app.listen(3000)
