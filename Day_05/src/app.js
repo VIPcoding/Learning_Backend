7090c08de33c492c4b616665459db4a62f840ddc
Vipin <vipsachdeva1@gmail.com>
2026-02-08 19:53:24 +0530

    Crud with Database

--- Day_05/src/app.js
+++ Day_05/src/app.js
@@ -1,7 +1,30 @@
 const express = require("express")
+const noteModel = require("./models/notes.models")
 
 const app = express()
+app.use(express.json())
 
+
+app.post("/notes", async (req, res)=>{
+    const {title, description} = req.body
+    const note = await noteModel.create({
+        title, description
+    })
+
+    res.status(201).json({
+        message: "Note created successfully",
+        note
+    })
+})
+
+
+app.get("/notes", async (req, res)=>{
+    const notes = await noteModel.find()
+
+    res.status(200).json({
+        message:"Note find Successfull",
+        notes
+    })
+})
 
 module.exports = app
-
